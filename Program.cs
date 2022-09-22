b3c5d4e0012451c3b8c2c05f59c0ccaa21eaf8b7
Anna <kretovaanna@mail.ru>
2022-09-22 09:01:27 +0500

    add Задача 2 (квадрат числа)

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Напишите программу, которая на вход принимает 
+2 числа и проверяет евяляется ли первое 
+число квадратом второго
+*/
+
+
+
+Console.Write("Введите число А:");
+int numberA = Convert.ToInt32(Console.ReadLine( ));
+
+Console.Write("Введите число В: ");
+int numberB = Convert.ToInt32(Console.ReadLine( ));
+
+int square = numberB * numberB;
+
+if (square == numberA)
+{
+    Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("нет");
+}
